58585fc7bba35049f06cec9c901fad7cd28836e3
catcreature <157812154+catcreature@users.noreply.github.com>
2024-12-20 04:37:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 name = input("What is your name?")
 print("Hello "+name)
+
+#Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data Types
+
+None
+
+# Fundamental Data Types
+int
+print(2+4)
+print(2-4)
+print(2*4)
+print(2/4)
+
+float
